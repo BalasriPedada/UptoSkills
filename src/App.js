54c93cf6f6f2710d937076b8fd3e30260a3ca407
unknown <mohanaswarupa2005@gmail.com>
2025-05-24 13:22:57 -0500

    Profiles Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Home  from './pages/Home';
 import  Register  from './pages/Register';
 import  Login  from './pages/Login';
+import Profiles from './pages/Profiles';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/register' element={<Register/>}/>
       <Route path='/login' element={<Login/>}/> 
+      <Route path='/profiles' element={<Profiles/>}/>
     </Routes>
     </BrowserRouter>
     </>
